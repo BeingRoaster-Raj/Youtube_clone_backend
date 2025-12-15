b5bea4906d481c1cb4edf79d572aaf4a0779542e
Raj Anand <ra9122325614@gmail.com>
2025-12-15 21:05:40 +0530

    add user and video model

--- Readme.md
+++ Readme.md
@@ -1,1 +1,30 @@
 Youtube backend trial and still trying with java script
+
+
+
+# Models -> Formed
+## User.model & video.model
+
+After that use or install mongoose-aggregate-paginate-v2  ---> this allows us to write aggregation qurries
+
+we will use this in video.model
+
+### Password Encryption
+
+now we will look towards some packages --> bcrypt(A bcrypt library for NodeJs) and bcryptjs(Optimized bcrypt in plain with zero dependencies. Compatible to "bcrypt". Mostly used).
+bcrypt => A library to help you hash password
+
+
+For token we will use jsonwebtoken --> It used to encrypt the data
+
+--> If you want that how tokens work then visit JWT.io
+
+
+After all this we cannot encrypt data directly so we will use some hooks of middleware - Pre(just before saving the data we can run any code by using this - usually password encryption),
+
+
+
+as we make middleware we can make methods  ref.- user.model
+
+
+jwt == it is a bearer token,it means this acts like a key(lock)
---
+++ src/models/user.model.js
@@ -0,0 +1,97 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+             type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url will be used
+            required: true
+        },
+        coverImage: {
+            type: String, // cloudinary url will be used
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"]   // encryption ka locha hai we will solve it later
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()  // ye check kar raha hai ki password field modify hua hai ya nahi
+    this.password = bcrypt.hash(this.password, 10)
+    next()   // whenever the data is being saved it will save password with it ---> so we have to modified it like whenever we send the modification of password field only then it should be hashed
+})
+// we have to create some methods so that we can ask the user whether the password is correct or not
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password)   // encrypted password ko compare karega
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model("User", userSchema)
+
+
