90bbeb78c02f994b18a264bed08af837c5014628
Raj Anand <ra9122325614@gmail.com>
2026-01-30 19:27:05 +0530

    Refactor user routes: add missing user account management and history retrieval endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,21 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser, updateUserAvatar, deleteUserAvatar } from "../controllers/user.controller.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateUserAvatar, 
+    deleteUserAvatar, 
+    changeCurrentUserPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload} from "../middlewares/Multer.middleware.js";
 import { veriftJWT } from "../middlewares/auth.middleware.js";
+import { get } from "mongoose";
 
 const router = Router()
 
@@ -23,10 +37,13 @@ router.route("/login").post(loginUser)
 // Secured Routes 
 router.route("/logout").post(veriftJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)  // to be implemented
-router.route("/update-avatar")
-.put(veriftJWT,upload.single("avatar"),updateUserAvatar)
-.post(veriftJWT,upload.single("avatar"),updateUserAvatar)  // to support both put and post methods in updating avatar
-
+router.route("/update-avatar").patch(veriftJWT,upload.single("avatar"),updateUserAvatar) // to support both put and post methods in updating avatar
 router.route("/delete-avatar").delete(veriftJWT,deleteUserAvatar) // delete user avatar route
+router.route("/cover-image").patch(veriftJWT,upload.single("/coverImage"), updateUserCoverImage)
+router.route("/change-password").post(veriftJWT,changeCurrentUserPassword) // change current user password route
+router.route("/current-user").get(veriftJWT,getCurrentUser)
+router.route("/update-account").patch(veriftJWT, updateAccountDetails) // update account details route
+router.route("/c/:username").get(veriftJWT, getUserChannelProfile) // get user channel profile by username route
+router.route("/history").get(veriftJWT, getWatchHistory)
 
 export default router
